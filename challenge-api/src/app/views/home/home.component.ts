760a10cd98278bf1055e817627db33b4ecc207e8
julicastro <castroejulian@gmail.com>
2024-01-08 03:36:29 -0300

    commit

--- challenge-api/src/app/views/home/home.component.ts
+++ challenge-api/src/app/views/home/home.component.ts
@@ -14,9 +14,8 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.climaService.obtenerDatosClima().subscribe((data) => {
-      
       this.datosClima = data;
-      console.log(data); 
+      console.log(data.name); 
     });
   }
 
